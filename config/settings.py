b82aeb0e65fa03b6fd5490fdf5c436d2887ee02f
xo4214 <40533858+xo4214@users.noreply.github.com>
2020-04-18 18:39:53 +0900

    d
    d

--- config/settings.py
+++ config/settings.py
@@ -37,9 +37,12 @@ DJANGO_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 ]
+THRID_PARTY_APPS = ["django_countries"]
 
 PROJECT_APPS = [
+    "core.apps.CoreConfig",
     "users.apps.UsersConfig",
+    "rooms.apps.RoomsConfig",
 ]
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS
 
